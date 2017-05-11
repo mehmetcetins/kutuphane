3e9b5d402b5040780347260e0257514c282c1eb8
Mehmet ÇETİN <Mehmet ETN>
2017-05-11 23:09:28 +0300

    ekleme sayfası tasarım

--- app/templates/book/view.php
+++ app/templates/book/view.php
@@ -1,44 +1,52 @@
 <form action="/kitap/ekle" method="post">
+            <div class="form-group">
+                <label>Kitap Adı:</label>
+                <input type="text" name="k_ad" required>
+            </div>
+            <div class="form-group">
+                <label>Yazarı:</label>
+                <input type="text" name="Yazar" required>
+            </div>
+            <div class="form-group">
+                <label>Yayıncısı:</label>
+                <input type="text" name="yayinci" required>
+            </div>
+            <div class="form-group">
+                <label>ISBN:</label>
+                <input type="text" name="isbn" required>
+            </div>
+            
+            <div class="form-group">
+                <label>Baskı Yılı:</label>
+                <input type="text" name="yili" required>
+            </div>
+            <div class="form-group">
+                <label>Nereden Geldi:</label>
+                <input type="text" name="kaynak" required>
+            </div>
+            <div class="form-group">
+                <label>Raf Yeri:</label>
+                <input type="text" name="konum" required>
+            </div>
+            <div class="form-group">
+                <label>Kapağı:</label>
+                <input type="text" name="kapak" required>
+            </div>
+            <div class="form-group">
+                <label>Özeti:</label>
+                <textarea name="ozet"  cols="30" rows="10" required></textarea>
+            </div>
+            <div class="form-group">
+                <label></label>
+                <input type="submit" value="EKLE">
+            </div>
 
-    <table border='1'>
-        <tr>
-            <td>Kitap Adı </td><td><input type="text" name="k_ad"></td>
-        </tr>
-        <tr>
-            <td>Yazarı</td><td><input type="text" name="yazar"></td>
-        </tr>
-        <tr>
-            <td>Yayıncısı</td><td><input type="text" name="yayinci"></td>
-        </tr>
-        <tr>
-            <td>ISBN</td><td><input type="text" name="isbn"></td>
-        </tr>
-        <tr>
-            <td>Özeti</td><td><textarea name="ozet" cols="30" rows="10"></textarea></td>
-        </tr>
-        <tr>
-            <td>Baskı Yılı</td><td><input type="text" name="yili"></td>
-        </tr>
-        <tr>
-            <td>Nereden Geldi</td><td><input type="text" name="kaynak"></td>
-        </tr>
-        <tr>
-            <td>Raf Yeri</td><td><input type="text" name="konum"></td>
-        </tr>
-        <tr>
-            <td>Kapağı</td><td><input type="text" name="kapak"></td>
-        </tr>
-        <tr>
-            <td colspan="2"><center><input type="submit" value="EKLE"></center>
                 <?php
                 if(isset($kitap))
                     echo "<font color='red'>$kitap</font>";
                 
                 ?>
-            </td>
 
-        </tr>
-    </table>
 
 
 </form>
--- app/templates/borrow/view.php
+++ app/templates/borrow/view.php
@@ -11,6 +11,9 @@ table td{
     color:#444;
     padding:10px;
 }
+table td:first-child {
+    width:240px;
+}
 table a{
     display:block;
     background:#333333;
@@ -47,7 +50,6 @@ if($_POST)
 {
     $query = $db->query(
     "SELECT *,odunc.id as odid FROM odunc INNER JOIN kitap ON odunc.kitap = kitap.id 
-    INNER JOIN ogrenci on odunc.ogrenci = ogrenci.numara
     where odunc.ogrenci=".$_POST["numara"],
     PDO::FETCH_OBJ
     );
@@ -62,9 +64,7 @@ if($_POST)
         <table cellpadding="0" cellspacing="0">
             <tr>
                 <th>kitap adı</th>
-                <th>öğrenci adı</th>
                 <th>Numarasi</th>
-                <th>Sınıfı</th>
                 <th>aldığınız tarih</th>
                 <th>teslim tarihi</th>
                 <th>teslim et</th>
@@ -72,9 +72,8 @@ if($_POST)
             <?php foreach($query as $qu):?>
              <tr>
                 <td><?=$qu->ad?></td>
-                <td><?=$qu->ogrenci_ad." ".$qu->soyad?></td>
-                <td><?=$qu->numara?></td>
-                <td><?=$qu->sinif?></td>
+                <td><?=$qu->ogrenci?></td>
+
                 <td><?=$qu->tarih?></td>
                 <td><?=$qu->teslim?></td>
                 <td><a href="/odunc/teslim?id=<?=$qu->odid?>">teslim et</a></td>
