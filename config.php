d00b83c670116494be6b717e693b5642be5b146c
İbrahim Duran <ibrahimeymenduran@gmail.com>
2017-05-12 19:25:45 +0300

    degisken ayarlar config.php den alindi

--- config.php
+++ config.php
@@ -2,6 +2,13 @@
 
 return [
   'APP' => [
-      'TEMPLATE_DIR' => 'app/templates'
-  ]  
+    'TEMPLATE_DIR' => 'app/templates'
+  ],
+  'DATABASE' => [
+    'HOST' => 'localhost',
+    'USER' => 'root',
+    'PASS' => '',
+    'NAME' => 'kutuphane',
+    'CHARSET' => 'utf8'
+  ]
 ];
--- init.php
+++ init.php
@@ -1,11 +1,18 @@
 <?php
 
+$config = require('config.php');
 $app = require('app/app.php');
 $url = '/' . $_GET['route'];
 
 $routes = [];
     try{
-        $db = new PDO("mysql:host=localhost;dbname=kutuphane;charset=utf8","root","");
+        $db = new PDO("mysql:
+            host={$config['DATABASE']['HOST']};
+            dbname={$config['DATABASE']['NAME']};
+            charset={$config['DATABASE']['CHARSET']}",
+            $config['DATABASE']['USER'],
+            $config['DATABASE']['PASS']
+        );
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e){
         $e->getMessage();
@@ -31,14 +38,16 @@ foreach ($app['resources'] as $route => $resource) {
 }
 
 function render($tpl, $vars = []) {
+    global $config;
+
     foreach ($vars as $name => $value) {
         $$name = $value;
     }
     
     ob_start();
-    include('app/templates/' . $tpl . '.php');
+    include($config['APP']['TEMPLATE_DIR'] . '/' . $tpl . '.php');
     $content = ob_get_contents();
     ob_end_clean();
 
-    include('app/templates/layout.php');
+    include($config['APP']['TEMPLATE_DIR'] . '/layout.php');
 }
